00e381621d763d2491e7fc01ffa3bafe2e99d754
Ashish Kumar Palai <ashishkumarpalai2000@gmail.com>
2022-11-02 22:33:29 +0530

    fixed and merge

--- prime.js
+++ prime.js
@@ -1,6 +1,14 @@
 function prime(num){
- let count=0;
-  for(let i;i<=num;i++){
-  if(num%i=0){
-  countee+;
+ let factor=0;
+  for(let i=0;i<=num;i++){
+  if(num%i==0){
+  factor++;
   }
+}
+if (factor==2){
+console.log("Prime")
+}else{
+console.log("Not Prime")
+}
+}
+prime(14);
